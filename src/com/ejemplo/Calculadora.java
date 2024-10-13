6feecf566a095b3793182bcb5aecec8ea20f4de6
Carol0161 <78099991+Carol0161@users.noreply.github.com>
2024-10-13 12:31:50 +0200

    Update Calculadora.java segundo comentario

--- src/com/ejemplo/Calculadora.java
+++ src/com/ejemplo/Calculadora.java
@@ -11,7 +11,7 @@ public class Calculadora {
 	}
 	
 	public int multiplicar(int a, int b) {
-		// comentario para detectar cambios dos
+		// comentario para detectar cambios tres
 		return a * b;
 	}
 	
